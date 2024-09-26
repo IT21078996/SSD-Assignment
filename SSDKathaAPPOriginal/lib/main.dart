0c644bdf8bb02114005cc971298ea6edf5ef409b
Sarindu <it21078996@my.sliit.lk>
2024-09-26 23:07:06 +0530

    meta data & content security header vulnerability fix

--- SSDKathaAPPOriginal/lib/main.dart
+++ SSDKathaAPPOriginal/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:katha/splashScreen.dart';
@@ -14,6 +13,19 @@ import 'Screens/ScreenTest/Correct.dart';
 import 'Screens/ScreenTest/InCorrect.dart';
 import 'Screens/ScreenTest/HowToSpeak.dart';
 import 'firebase_options.dart';
+import 'package:http/http.dart' as http;
+
+void fetchMetaData() async {
+  final response = await http.get(Uri.parse('http://169.254.169.254/latest/meta-data/instance-id'));
+
+  if (response.statusCode == 200) {
+    // If the server returns a 200 OK response, parse the metadata
+    print('Metadata: ${response.body}');
+  } else {
+    // If the server returns an unsuccessful response code, throw an exception.
+    throw Exception('Failed to load metadata');
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
